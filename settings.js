7757ac89decd4f71d09f9eabaed825a05271a824
Dark-Mystic <asifofc615@gmail.com>
2023-06-23 14:41:20 +0500

    first commit

---
+++ settings.js
@@ -0,0 +1,71 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['923474187615'] //ur owner number
+global.ownernomer = "923474187615" //ur owner number2
+global.ownername = "Asif khan" //ur owner name
+global.ytname = "YT: " //ur yt chanel name
+global.socialm = "GitHub: " //ur github or insta name
+global.location = "Pakistan,BWP" //ur location
+
+//new
+global.botname = "Pikachu-Bot"
+global.ownernumber = '923474187615'
+global.ownername = 'Asif Khan'
+global.ownerNumber = ["923474187615@s.whatsapp.net"]
+global.ownerweb = "https://wa.me/+923474187615"
+global.websitex = "https://wa.me/+923474187615"
+global.wagc = "https://chat.whatsapp.com"
+global.themeemoji = '🫂'
+global.wm = "Pikachu Bot Inc."
+global.botscript = 'https://wa.me/+923474287615' //script link
+global.packname = "Pikachu-Bot"
+global.author = "Asif khan"
+global.creator = "923474187615@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./Pikachu/theme/Pikachu.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./Pikachu/theme/Pikachu.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./Pikachu/theme/Pikachu.jpg") //ur error pic
+global.thumb = fs.readFileSync("./Pikachu/theme/Pikachu.jpg") //ur thumb pic
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: '𝐒𝐮𝐜𝐜𝐞𝐬𝐬!',
+    admin: '𝐓𝐡𝐢𝐬 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐜𝐨𝐮𝐥𝐝 𝐛𝐞 𝐮𝐬𝐞𝐝 𝐛𝐲 𝐚𝐝𝐦𝐢𝐧𝐬 𝐨𝐧𝐥𝐲!',
+    botAdmin: '𝐁𝐨𝐭 𝐌𝐮𝐬𝐭 𝐁𝐞 𝐀𝐝𝐦𝐢𝐧 𝐅𝐢𝐫𝐬𝐭!',
+    premime: '𝐏𝐫𝐞𝐦𝐢𝐮𝐦 𝐒𝐩𝐞𝐜𝐢𝐚𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐭𝐨 𝐑𝐞𝐠𝐢𝐬𝐭𝐞𝐫 𝐓𝐲𝐩𝐞 𝐑𝐞𝐧𝐭',
+    owner: '𝐓𝐡𝐢𝐬 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐜𝐨𝐮𝐥𝐝 𝐛𝐞 𝐮𝐬𝐞𝐝 𝐛𝐲 𝐎𝐰𝐧𝐞𝐫 𝐨𝐧𝐥𝐲',
+    group: '𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐔𝐬𝐞𝐝 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐆𝐫𝐨𝐮𝐩𝐬!',
+    private: '𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐔𝐬𝐞𝐝 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 𝐂𝐡𝐚𝐭!',
+    bot: '𝐓𝐡𝐢𝐬 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐜𝐨𝐮𝐥𝐝 𝐛𝐞 𝐮𝐬𝐞𝐝 𝐛𝐲 𝐙𝐞𝐧𝐢𝐭𝐬𝐮-𝐁𝐨𝐭 𝐨𝐧𝐥𝐲!',
+    wait: '𝐖𝐚𝐢𝐭...',
+    linkm: '𝐖𝐡𝐞𝐫𝐞 𝐢𝐬 𝐭𝐡𝐞 𝐥𝐢𝐧𝐤?',
+    endLimit: '𝐘𝐨𝐮𝐫 𝐃𝐚𝐢𝐥𝐲 𝐋𝐢𝐦𝐢𝐭 𝐇𝐚𝐬 𝐄𝐱𝐩𝐢𝐫𝐞𝐝, 𝐓𝐡𝐞 𝐋𝐢𝐦𝐢𝐭 𝐖𝐢𝐥𝐥 𝐁𝐞 𝐑𝐞𝐬𝐞𝐭 𝐄𝐯𝐞𝐫𝐲 𝟏𝟐 𝐇𝐨𝐮𝐫𝐬',
+    nsfw: '𝐓𝐡𝐞 𝐧𝐬𝐟𝐰 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐡𝐚𝐬 𝐧𝐨𝐭 𝐛𝐞𝐞𝐧 𝐚𝐜𝐭𝐢𝐯𝐚𝐭𝐞𝐝, 𝐩𝐥𝐞𝐚𝐬𝐞 𝐜𝐨𝐧𝐭𝐚𝐜𝐭 𝐭𝐡𝐞 𝐚𝐝𝐦𝐢𝐧 𝐭𝐨 𝐚𝐜𝐭𝐢𝐯𝐚𝐭𝐞(𝐓𝐲𝐩𝐞 𝐍𝐬𝐟𝐰 𝐨𝐧)𝐭𝐨 𝐚𝐜𝐭𝐢𝐯𝐚𝐭𝐞',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
